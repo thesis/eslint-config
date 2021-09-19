f569ff5e46f9c9c8f8eab2f3e45ebe602e87be73
Matt Luongo <mhluongo@gmail.com>
2021-09-18 21:14:07 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+// This is a JS file, so this rule can't be followed.
+/* eslint-disable @typescript-eslint/no-var-requires */
+module.exports = {
+  extends: [require.resolve("./index.js")],
+}
+/* eslint-enable @typescript-eslint/no-var-requires */
---
+++ index.js
@@ -0,0 +1,48 @@
+// This is a JS file, so this rule can't be followed.
+/* eslint-disable @typescript-eslint/no-var-requires */
+const {
+  rules: { "no-param-reassign": airbnbNoParamReassignRules },
+} = require("eslint-config-airbnb-base/rules/best-practices")
+/* eslint-enable @typescript-eslint/no-var-requires */
+
+module.exports = {
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  globals: {
+    document: "readonly",
+    window: "readonly",
+  },
+  rules: {
+    semi: "never",
+    "import/no-extraneous-dependencies": [
+      "error",
+      // Don't slap build files for importing devDependencies.
+      { devDependencies: ["!+(src/api|ui)/**/*.+(ts|js)"] },
+    ],
+    // Add known-safe exceptions to no-param-reassign.
+    "no-param-reassign": [
+      airbnbNoParamReassignRules[0],
+      {
+        props: airbnbNoParamReassignRules[1].props,
+        ignorePropertyModificationsFor:
+          airbnbNoParamReassignRules[1].ignorePropertyModificationsFor,
+        ignorePropertyModificationsForRegex: [
+          ...(airbnbNoParamReassignRules[1]
+            .ignorePropertyModificationsForRegex || []),
+          "^immer", // For redux-toolkit reducers using immer.
+        ],
+      },
+    ],
+  },
+  ignorePatterns: ["dist/"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig-eslint.json",
+  },
+}
